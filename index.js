01e356443164d8b28feb23a96ee00d707a5a6144
Kishlay Raj <kishlayrajiem@gmail.com>
2017-06-14 14:54:45 +0530

    adding hard coded number of households in rajasthan for check

--- index.js
+++ index.js
@@ -13,44 +13,7 @@ restService.post('/hook', function (request, response) {
 
     console.log('hook request');
 
-    /*try {
-        var speech = 'empty speech1111';
-
-        if (req.body) {
-            var requestBody = req.body;
-
-            if (requestBody.result) {
-                speech = '';
-
-                if (requestBody.result.fulfillment) {
-                    speech += requestBody.result.fulfillment.speech;
-                    speech += ' ';
-                }
-
-                if (requestBody.result.action) {
-                    speech += 'action: ' + requestBody.result.action;
-                }
-            }
-        }
-
-        console.log('result: ', speech);
-
-        return res.json({
-            speech: speech,
-            displayText: speech,
-            source: 'apiai-webhook-sample'
-        });
-    } catch (err) {
-        console.error("Can't process request", err);
-
-        return res.status(400).json({
-            status: {
-                code: 400,
-                errorType: err.message
-            }
-        });
-    }*/
-	
+   
 	 const app = new App({request, response});
   console.log('Request headers: ' + JSON.stringify(request.headers));
   console.log('Request body: ' + JSON.stringify(request.body));
@@ -61,8 +24,8 @@ restService.post('/hook', function (request, response) {
     app.ask('Hello, World!');
   }
 
-//return app.ask("who are you");
-return responseHandler (app);
+return app.ask("the number of households in rajasthan is 201");
+//return responseHandler (app);
   const actionMap = new Map();
   actionMap.set('<API.AI_action_name>', responseHandler);
 
