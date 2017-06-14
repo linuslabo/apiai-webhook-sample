325954ae0183aafaedc4f67d16da91a83a93089b
Kishlay Raj <kishlayrajiem@gmail.com>
2017-06-14 13:09:59 +0530

    new index file with library

--- index.js
+++ index.js
@@ -3,16 +3,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+process.env.DEBUG = 'actions-on-google:*';
+const App = require('actions-on-google').ApiAiApp;
 
 const restService = express();
 restService.use(bodyParser.json());
 
-restService.post('/hook', function (req, res) {
+restService.post('/hook', function (request, response) {
 
     console.log('hook request');
 
-    try {
-        var speech = 'empty speech';
+    /*try {
+        var speech = 'empty speech1111';
 
         if (req.body) {
             var requestBody = req.body;
@@ -47,7 +49,24 @@ restService.post('/hook', function (req, res) {
                 errorType: err.message
             }
         });
-    }
+    }*/
+	
+	 const app = new App({request, response});
+  console.log('Request headers: ' + JSON.stringify(request.headers));
+  console.log('Request body: ' + JSON.stringify(request.body));
+
+  // Fulfill action business logic
+  function responseHandler (app) {
+    // Complete your fulfillment logic and send a response
+    app.ask('Hello, World!');
+  }
+
+//return app.ask("who are you");
+return responseHandler (app);
+  const actionMap = new Map();
+  actionMap.set('<API.AI_action_name>', responseHandler);
+
+  app.handleRequest(actionMap);
 });
 
 restService.listen((process.env.PORT || 5000), function () {
